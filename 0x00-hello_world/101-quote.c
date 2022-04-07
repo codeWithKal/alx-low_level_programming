335ef67e78fa3c716f4488385ee9403e69081c81
kalu_coder <awesomecoding51@gmail.com>
2022-04-07 16:48:02 -0700

    bug fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
-
 /**
- * main - prints a string without using either printf or puts
- * @void: doesn't  take any argument
+ * main - prints to the standard error without using printf and pust
+ * @void: no parameter 
  *
  * Return: 1
  */
 int main(void)
 {
 	write(2,
-              "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
-	  	59);
+	      "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+	      59);
 	return (1);
 }
