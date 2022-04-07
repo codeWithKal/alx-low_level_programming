4a69da588111efa87468fcfa85a132d73d4a29d6
kalu_coder <awesomecoding51@gmail.com>
2022-04-07 07:10:38 -0700

    printing without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a string without using either printf or puts
+ * @void: doesn't  take any argument
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that  piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
