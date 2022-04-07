3a109cda5a3add2e893b1e8fab0935100d532368
kalu_coder <awesomecoding51@gmail.com>
2022-04-07 05:58:25 -0700

    betty style corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - prints "Programming  is like  building a multilingual puzzle"
  * @void: makes no parameter intake
- * Return : 0
+ * Return : thies program doesn't seem to return anything
 */
- int main(void)
-{ 
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
