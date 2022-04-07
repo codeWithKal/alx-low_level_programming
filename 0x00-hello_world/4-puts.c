2914421ea08b118beea7e1add207023bc1a67c82
kalu_coder <awesomecoding51@gmail.com>
2022-04-07 06:07:56 -0700

    output corrected and documentation stated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - prints "Programming  is like  building a multilingual puzzle"
  * @void: makes no parameter intake
- * Return : thies program doesn't seem to return anything
+ *
+ * Return : this program doesn't seem to return anything
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
