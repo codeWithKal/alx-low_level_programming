2c2499bc0dbe834dbeac478e06ae5616dcb9944e
kalu_coder <awesomecoding51@gmail.com>
2022-04-07 05:42:09 -0700

    applying betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints "Programming  is like  building a multilingual puzzle"
+ * @void: makes no parameter intake
+ * Return : 0
 */
  int main(void)
 { 
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
