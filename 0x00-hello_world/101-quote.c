3524f3b0462b3df51b279870fed12d5cec6644d7
kalu_coder <awesomecoding51@gmail.com>
2022-04-07 16:55:48 -0700

    betty style changed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,7 @@
 
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints to the standard error without using printf and pust
  * @void: no parameter 
@@ -9,7 +11,7 @@
 int main(void)
 {
 	write(2,
-	      "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+	        "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
 	      59);
 	return (1);
 }
