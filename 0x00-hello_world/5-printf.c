bdaf72009e459bee821e382dbbb326d18adb8b8e
kalu_coder <awesomecoding51@gmail.com>
2022-04-07 06:16:18 -0700

    function using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a sentence using printf method
+ * @void: takes nothing as a parameter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
