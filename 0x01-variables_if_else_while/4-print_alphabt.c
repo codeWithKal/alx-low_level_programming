9680d64c306c7dbf13fcae3c0cd6a35ffdcae69c
kalu_coder <awesomecoding51@gmail.com>
2022-04-08 11:52:15 -0700

    while is replaced with if

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,8 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		while !(ch == 'q' || ch == 'e')
-		putchar(ch);
+		if (ch != 'q' || ch != 'e')
+			putchar(ch);
 	putchar('\n');
 	return (0);
 }
