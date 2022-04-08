b5c97be42bf38b09dd7b349b2c03a17c1093d20e
kalu_coder <awesomecoding51@gmail.com>
2022-04-08 08:55:03 -0700

    code modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,13 @@
 #include <stdio.h>
 /**
  * main - prints to the standard error without using
- * @void: no parameter 
+ * @void: no parameter:
  *
  * Return: 1
  */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
-			59);
+	       "and that piece of art is useful" - Dora Korpar, 2015-10-19,59);
 	return (1);
 }
