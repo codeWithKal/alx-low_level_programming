d05fa880bb6cd4f67217ce7932c88246d43ffe7b
kalu_coder <awesomecoding51@gmail.com>
2022-04-08 11:15:12 -0700

    printing lower and upper case letters intermixing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: doesn't take any parameter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
