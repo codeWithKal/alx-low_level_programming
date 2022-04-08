001f7c373eb69bbbd041f8cffc874bc01bc178e3
kalu_coder <awesomecoding51@gmail.com>
2022-04-08 13:28:06 -0700

    printing hexadecimals using putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints hexadecimal letter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+}
