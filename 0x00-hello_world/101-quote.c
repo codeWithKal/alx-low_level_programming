62341265cb3517b1d4289cc0a24bcdccf2d5f772
kalu_coder <awesomecoding51@gmail.com>
2022-04-07 17:34:44 -0700

    white spaces are avoided

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
 /**
- * main - prints to the standard error without using printf and pust
+ * main - prints to the standard error without using
  * @void: no parameter 
  *
  * Return: 1
  */
 int main(void)
 {
-	write(2, 
-			"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
-		       	59);
+	write(2, "that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
