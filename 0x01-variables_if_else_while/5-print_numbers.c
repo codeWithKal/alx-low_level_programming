23de3ccbdad30114da0876de8fb6374f79ad2d67
kalu_coder <awesomecoding51@gmail.com>
2022-04-08 12:04:36 -0700

    displaying a base 10 digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void - no parameter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 0; i <= 10; i++)
+	{
+		printf(i);
+		printf('\n');
+	}
+	return (0);
+}
