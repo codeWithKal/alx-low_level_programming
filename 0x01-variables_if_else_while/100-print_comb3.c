f3188664e73aeda83f4e9e38ee55da7dab77732f
kalu_coder <awesomecoding51@gmail.com>
2022-04-08 16:39:49 -0700

    betty style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,18 @@ int main(void)
 	for (n = 48; n <= 56; n++)
 	{
 		for (m = 49; m <= 57; m++)
-			{if (m > n)																			{
-					putchar(n);putchar(m);
-																								if (n != 56 || m != 57)
-																								{
-																									putchar(',');
-																									putchar(' ');																																			}
-																						}
-																	}
-									}
-				putchar('\n');
-					return (0);
+		{
+			if (m > n)							           {
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					char(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
