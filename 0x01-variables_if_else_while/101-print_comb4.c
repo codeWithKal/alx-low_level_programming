5b689f18982abe8cc50037cf6c4d99acb9c4b0b7
kalu_coder <awesomecoding51@gmail.com>
2022-04-08 17:02:33 -0700

    betty style applied

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - main block
- *   * Description: Write a program that prints all possible
- *    * Numbers must be separated by ,, followed by a space
- *     * prints combination of 3 digits.
- *      *
- *       * Return: 0
- *        */
+ *main - main block
+ *Description: Write a program that prints all possible
+ *Numbers must be separated by ,, followed by a space
+ *
+ *prints combination of 3 digits.
+ *Return: 0
+ */
 int main(void)
 {
 	int c;
@@ -34,12 +34,10 @@ int main(void)
 					}
 				}
 				c++;
-			} 
+			}
 			d++;
 		}
-		e++;		
-	}
-						
+		e++;				
 	putchar('\n');
 	return (0);
 }
