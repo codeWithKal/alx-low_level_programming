228800abdf6cbb1293f8a9d8a09535d54981ed40
kalu_coder <awesomecoding51@gmail.com>
2022-04-12 13:20:10 -0700

    adding two operands

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two number
+ * @x: operand to be added
+ * @y: operand to be added
+ * Return: integer value
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	_putchar(sum);
+	return (0);
+}
