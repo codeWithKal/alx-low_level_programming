331e452112b1e58a098768109070c45b83d98ab5
kalu_coder <awesomecoding51@gmail.com>
2022-04-12 11:08:06 -0700

    lowercase checker

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** _islower - checks for the case of a letter
+ * @parameter: takes an integral parameter c
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
