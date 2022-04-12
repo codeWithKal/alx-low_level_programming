1add4ed5ecd1c0e97ffa01eaf707c180f079d5c9
kalu_coder <awesomecoding51@gmail.com>
2022-04-12 11:39:03 -0700

    documentation solved

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
  * print_sign - prints signs of a number
  * @n: takes integer paramer n
  * Return: 1 if the number is positive
-	0 if the number is zero
-	-1 if the number is negative
+ * 0 if the number is zero
+ * -1 if the number is negative
  */
 int print_sign(int n)
 {
