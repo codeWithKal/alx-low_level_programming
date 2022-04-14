ea8f22920a1e43d772cb18c19c1e58214a61dc12
kalu_coder <awesomecoding51@gmail.com>
2022-04-15 01:49:47 +0300

    triangle maker

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,32 @@
-#include "main.c"
+#include "main.h"
+
 /**
- * print_triangle - prints a right angled triangle
- * @size: is the size of the triangle
+ * print_triangle - prints triangle
+ * @size: limit
+ *
+ * Return: void
  */
+
 void print_triangle(int size)
 {
-	int row, culumn, filler;
+int i, j;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= size; row++)
-		{
-			for (column = 1; column <= size; column++)
-			{
-				if ((size - i - 1) > j)
-					_putchar(' ');
-				else
-					_putchar('#');
-				_putchar('\n');
-			}
-		}
-	}
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if ((size - i - 1) > j)
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
