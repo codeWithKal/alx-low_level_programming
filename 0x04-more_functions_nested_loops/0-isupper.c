003d13262c2bbe48dfc048290adbf04f28792918
kalu_coder <awesomecoding51@gmail.com>
2022-04-15 02:56:50 +0300

    case checker

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,20 @@
-#include "main.c"
+#include "main.h"
 /**
- * _isupper - checkes for the case of letters
- * @c: c parameter
- * Return: 1 if the character is uppercase or 0 otherwise
+ * _isupper - checkes upper case
+ * @c: print int
+ * Return: give 1 or 0
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
 }
