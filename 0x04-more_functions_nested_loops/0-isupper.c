97f92144e24a98067cf31f379f03dbb785c7788a
kalu_coder <awesomecoding51@gmail.com>
2022-04-14 21:35:24 +0300

    character checker

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.c"
+/**
+ * _isupper - checkes for the case of letters
+ * @c: c parameter
+ * Return: 1 if the character is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
