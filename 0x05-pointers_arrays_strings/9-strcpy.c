7c69ef96633baa4e1e87cabd83f89f42f8efb985
kalu_coder <awesomecoding51@gmail.com>
2022-04-19 00:00:46 +0300

    copying strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcpy - a functions that return a character pointer
+ * @dest: a character pointer variable
+ * @src: a character pointer variable
+ * Return: a character pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (*dest);
+}
