d405222e0bbf980c85e6176b310f6d17b939487d
kalu_coder <awesomecoding51@gmail.com>
2022-04-18 22:50:13 +0300

    characters and new lines

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every character with a new line
+ * @str: a character pointer
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		_putchar(s[i]);
+		_putchar('\n');
+	}
+}
