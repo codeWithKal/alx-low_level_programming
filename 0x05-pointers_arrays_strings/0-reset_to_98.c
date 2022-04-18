57cc500e484c8cf56e07aa52a20780fb27afb77c
kalu_coder <awesomecoding51@gmail.com>
2022-04-18 20:54:06 +0300

    main function discarded

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,19 +8,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-/**
- * main - starting method
- * Return: nothing
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	_putchar('n');
-	_putchar('=');
-	_putchar(n);
-	reset_to_98(&n);
-	_putchar('n');
-	_putchar('=');
-	_putchar(n);
