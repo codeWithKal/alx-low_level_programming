9f82a8f8ed1bf846abc2c6a229dc6bc0e54cff1b
kalu_coder <awesomecoding51@gmail.com>
2022-04-20 22:42:29 +0300

    escape character changed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,15 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\n')
+	while (dest[i] != '\0')
 		i++;
 	j = 0;
-	while (src[j] != '\n')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
-	dest[i] = '\n';
+	dest[i] = '\0';
 	return (dest);
 }
