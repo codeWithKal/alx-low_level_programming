1336edbd2e9f0718e13c4f1b299f1a87466c9956
kalu_coder <awesomecoding51@gmail.com>
2022-04-27 21:02:49 +0300

    string printer

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,9 +4,13 @@
  * @s: a character pointer
  * Return: nothing
  */
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
-	_putchar(s[0]);
-	_puts_recursion(s + 1);
-	_putchar('\n');
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
