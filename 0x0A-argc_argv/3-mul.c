14fb9cde8d968fcfb5a841c77d0a564e149636dc
kalu_coder <awesomecoding51@gmail.com>
2022-04-29 19:46:11 +0300

    operand arguments given a name

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,16 +7,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul;
+	int mul, x, y;
 
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		mul = x * y;
 		printf("%d\n", mul);
 		return (0);
 	}
