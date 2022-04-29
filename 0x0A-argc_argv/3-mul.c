28db57690764da4c0692c38ff207296ccd693f3b
kalu_coder <awesomecoding51@gmail.com>
2022-04-29 19:40:59 +0300

    code reconstracted

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,15 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	argc = argc;
-
-	if (argv[1] != NULL && argv[2] != NULL)
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
 		return (0);
 	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
 }
-
-
