e290a3e253e921ccf6c538242c2d298e901a3424
kalu_coder <awesomecoding51@gmail.com>
2022-05-07 01:59:30 +0300

    the other taks added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * strlen - computes the length of a string
- * @s: the string parameter
- * Return: the length of string s
- */
-unsigned int strlen(char *s)
-{
-	unsigned int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
-}
-
-/**
- * string_nconcat - concatenates two strings by a constraint of
- * n byte of the second string
- * @s1: the first string pointer to be concatenated
- * @s2: the second string pointer to be concatedanted
- * @n: the number of byte of second string to be concatenated
- * Return: a pointer to new concatenatted string
+ * string_nconcat - concatenates two strings with a limit
+ * @s1: first string
+ * @s2: second string
+ * @n: limit of s2
+ * Return: pointer to new space in memory or null
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *final;
-	int i, j;
+int s;
+unsigned int t;
+char *strDup;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+	s = 0;
+	while (s1[s] != '\0')
+		s++;
 
-	if (n <= strlen(s2))
-		final = malloc(strlen(s1) + n + 1);
-	else
-		final = malloc(strlen(s1) + strlen(s2) + 1);
-	if (final == 0)
+	strDup = malloc(sizeof(char) * (s + n + 1));
+	if (strDup == NULL)
+	{
+		free(strDup);
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		final[i] = s1[i];
-	for (j = 0; s2[j]; j++, i++)
-		final[i] = s2[j];
-	final[i] = '\0';
-	return (final);
+	}
+	s = t = 0;
+	while (s1[s] != '\0')
+	{
+		strDup[s] = s1[s];
+		s++;
+	}
+	while (s2[t] != '\0' && t < n)
+	{
+		strDup[s] = s2[t];
+		s++, t++;
+	}
+	strDup[s] = '\0';
+	return (strDup);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,9 +3,9 @@
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-unsigned int strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int multiply(int argc, char *argv[]);
 
-
-#endif /* end of the header */
+#endif
