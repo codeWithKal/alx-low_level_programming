0fdf46bc9e7b699ed1487b5cfcdce82cd4ba5e10
kalu_coder <awesomecoding51@gmail.com>
2022-05-06 19:22:43 +0300

    header file and task one added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates a memory size of a parameter
+ * @b: the size of memory in bytes to be allocated by malloc
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *c;
+
+	c = malloc(b);
+	if (c == NULL)
+	{
+		exit(98);
+	}r
+	return (c);
+}
