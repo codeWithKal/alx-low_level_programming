fd58b8bf9a012b1633e4912efc85508bcc8258bf
kalu_coder <awesomecoding51@gmail.com>
2022-05-09 21:46:52 +0300

    code debugged

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = &name;
+	if (d == NULL)
+		return;
+	else
+	{
+	d->name = name;
 	d->age  = age;
-	d->owner = &owner;
+	d->owner = owner;
+	}
 }
