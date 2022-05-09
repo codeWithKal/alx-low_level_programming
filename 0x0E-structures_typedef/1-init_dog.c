47d0fcdce5feda0f25f623cce847d290d2ed395c
kalu_coder <awesomecoding51@gmail.com>
2022-05-09 21:39:29 +0300

    code completed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "dog.h"
 /**
  * init_dog - inintialized the struct dog
  * @d: a parameter of structure
@@ -8,4 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+	d->name = &name;
+	d->age  = age;
+	d->owner = &owner;
+}
