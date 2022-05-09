99f9642a03f48ee47c4547b7851de0e8452fd906
kalu_coder <awesomecoding51@gmail.com>
2022-05-09 21:03:53 +0300

    new project of the day - readme, header file and task 0 are added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * struct dog - a structure for dog
+ * @name: a num of the dog
+ * @age: is an age of dog
+ * @owner: the owner of a dog
+ * Description: this struct defines a new struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
