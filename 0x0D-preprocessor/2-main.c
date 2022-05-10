0825f9482412b46ef8ff7c69805788676e372fdd
kalu_coder <awesomecoding51@gmail.com>
2022-05-10 20:22:34 +0300

    macro name is corrected

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-#define PARENT_FILE _FILE_
+#define PARENT_FILE __FILE__
 
 /**
  * name_caller - calles the source file name
  * Return: nothing
  */
-void name_caller()
+void name_caller(void)
 {
 	printf("%s", PARENT_FILE);
 }
