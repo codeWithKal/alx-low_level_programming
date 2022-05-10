9556e4b88565cef7f4c3c182053af0e4143bbad5
kalu_coder <awesomecoding51@gmail.com>
2022-05-10 20:36:23 +0300

    header file and source file are editted

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 #define PARENT_FILE __FILE__
-
-/**
- * name_caller - calles the source file name
- * Return: nothing
- */
-void name_caller(void)
-{
-	printf("%s", PARENT_FILE);
-}
-
 /**
  * main - calles the name_caller function
  * Return: 0 (always success)
  */
 void main(void)
 {
-	name_caller();
+	printf("%s\n", PARENT_FILE);
 }
--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void name_caller(void);
 void main(void);
 
 #endif
