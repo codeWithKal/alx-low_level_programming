f693c846be62df882945d6db786a1f82336c13f3
kalu_coder <awesomecoding51@gmail.com>
2022-05-11 23:00:01 +0300

    function caller over a different parameter of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_iterator - calles a function on differnt values of an array paramters
+ * @array: an array parameter
+ * @size: size of an array
+ * @action: a pointer to the function we gonna use
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !action)
+		return;
+	for (i = 0; i < size; i++)
+		action(i);
+}
