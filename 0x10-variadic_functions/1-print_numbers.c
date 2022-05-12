4b1fd7c50beac7544e726643cdfaa5187a630a7d
kalu_coder <awesomecoding51@gmail.com>
2022-05-12 23:15:18 +0300

    function name corrected

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,11 @@
 #include "variadic_functions.h"
 /**
- * print_strings - prints a string, followed by a new line
+ * print_number - prints a string, followed by a new line
  * @separator: a string to be printed between the strings
  * @n: the number of strings passed to the function
  * Return: nothing
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_number(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
