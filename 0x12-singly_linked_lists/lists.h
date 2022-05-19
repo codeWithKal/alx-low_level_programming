1a5bc14be55238114460fe42031fac974753642f
kalu_coder <awesomecoding51@gmail.com>
2022-05-19 20:14:50 +0300

    struct is defined in the header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
+/**
+ * struct mynode - a structure defined for creating custom nodes
+ * @len: numerica data of a node
+ * @str: a string data of a node
+ * @next: a pinter to the next node
+ */
 
+typedef struct mynode
+{
+	int len;
+	char *str;
+	struct mynode *next;
+}list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
