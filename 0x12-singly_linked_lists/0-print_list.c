7dcb7213e6f3e0ad543d37bffbe9abd893ee391a
kalu_coder <awesomecoding51@gmail.com>
2022-05-19 19:53:49 +0300

    header file, readmefile and task 0 of the project are pushed

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of a list_t
+ * @h: a pinter to list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int node = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil) \n");
+		}
+		else
+		{
+			printf("[%u] %s \n", h->len, h->str);
+		}
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+#singly linked lists
+This subrepository contains a project tasks for singly linked list data structure.
