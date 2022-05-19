0cd85ede505669e16c385a3abbadb9100f8d28bb
kalu_coder <awesomecoding51@gmail.com>
2022-05-19 21:48:36 +0300

    element counter

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * list_len - a function that returns a length of the linked list
+ * @h: the linked list pointer
+ * Return: the length of the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int node;
+
+	for (node = 0; h->next != NULL; node++)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
