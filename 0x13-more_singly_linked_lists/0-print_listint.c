1eac4a2510dc50e2699b9326826639b78465bad1
kalu_coder <awesomecoding51@gmail.com>
2022-05-23 13:53:59 +0300

    list element displayer

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+/**
+ * print_listint - prints the elements of a list.
+ * @h: a pointer to the linked list
+ * Return: a number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int counter = 0;
+
+	for (counter = 0; h != NULL; counter++)
+	{
+		printf("%d", h->n);
+		h = h->next;
+	}
+	return (counter);
+}
