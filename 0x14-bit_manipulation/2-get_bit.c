ea6877a8ccb70b9ee104f6a28a67e29121e27c07
kalu_coder <awesomecoding51@gmail.com>
2022-05-27 19:17:09 +0300

    return value of bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - gets bit on index `index`
+ * @n: decimal number
+ * @index: index of the bit
+ * Return: the bit found
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * BYTE_LENGHT)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
+© 2022 GitHub, Inc.
+
