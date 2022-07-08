08ec2f8199fadfd40c7c46da6d5d4e8574244639
kalu_coder <awesomecoding51@gmail.com>
2022-07-08 20:58:27 +0300

    if statement is added to return NULL'g

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,8 +11,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
+	if (!new_node)
+		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
+
 
 	if (*head == NULL)
 	{
