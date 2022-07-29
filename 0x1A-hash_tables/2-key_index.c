25e869ca86109e699cd14de7733fae730391c4ba
kalu_coder <awesomecoding51@gmail.com>
2022-07-29 11:46:35 +0300

    initial commit to the index returner of a key using djb2 algo

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hush_tables.h"
+/*
+ * key_index - a function that gived an index of a character key
+ *
+ * @key: a pointer to the key string
+ * @size: size of a hash table
+ *
+ * Return: index of a key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) & size);
+}
