de1d002b1751674d32ab7067748e50ce21a5b272
kalu_coder <awesomecoding51@gmail.com>
2022-07-29 10:44:47 +0300

    task 0 debugged according to the changed is header file.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int index = 0;
+	unsigned long int i;
 
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
-	if (new_table == NULL)
-		return (NULL);
-	new_table->size = size;
-	if (new_table->size == NULL)
-		return (NULL);
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-
-	for (; index < size; index++)
+	if (new_table != NULL)
 	{
-		new_table->array[index] = NULL;
+		new_table->size = size;
+		new_table->array = malloc(sizeof(hash_node_t *) * size);
+		if (new_table->array != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				new_table->array[i] = NULL;
+			}
+		}
 	}
 	return (new_table);
 }
